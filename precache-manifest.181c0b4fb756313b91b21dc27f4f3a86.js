35fe11d0035bc8618bdbed04faa1a1e9e7512343
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2019-10-24 19:03:58 -0300

    Updates

---
+++ precache-manifest.181c0b4fb756313b91b21dc27f4f3a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5147568486177f11f487720a474d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce1d2e5944ee9d3877b",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "acf34be24d427a93efeb",
+    "url": "/static/css/main.10a69a8c.chunk.css"
+  },
+  {
+    "revision": "4ce1d2e5944ee9d3877b",
+    "url": "/static/js/2.b5fabe04.chunk.js"
+  },
+  {
+    "revision": "acf34be24d427a93efeb",
+    "url": "/static/js/main.3c98a424.chunk.js"
+  },
+  {
+    "revision": "54380f5369b2add4714f",
+    "url": "/static/js/runtime-main.4ef65144.js"
+  }
+]);
