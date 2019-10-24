4616497887383f407f87ef7c3e4da75cbb4006fb
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2019-10-24 19:15:15 -0300

    Removendo log

--- src/store/actions/company.js
+++ src/store/actions/company.js
@@ -3,7 +3,6 @@ import { api } from '../../utils'
 export const loadCompanies = () => {
   return dispatch => {
     api.get('companies').then(result => {
-      console.log(result)
       let data = []
       if (result.success) {
         data = result.data
