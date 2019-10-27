b96a88453eca371d399233f491514a653fba38f1
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2019-10-27 11:03:56 -0300

    Updates

--- precache-manifest.1571445df8d9c95891350b3584455b5c.js
+++ precache-manifest.1571445df8d9c95891350b3584455b5c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ee8d45722f76b2b3fea4fdcf9da622f",
+    "revision": "9479cb9976d6b1133f9aed45ea14796a",
     "url": "/b3-companies-webapp/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/b3-companies-webapp/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "cb0c15a5f82bcae8d70f",
-    "url": "/b3-companies-webapp/static/css/main.10a69a8c.chunk.css"
+    "revision": "b69a5683430d51a61fc1",
+    "url": "/b3-companies-webapp/static/css/main.27e539d3.chunk.css"
   },
   {
     "revision": "9eabce57a6c7cf975672",
     "url": "/b3-companies-webapp/static/js/2.0ce24377.chunk.js"
   },
   {
-    "revision": "cb0c15a5f82bcae8d70f",
-    "url": "/b3-companies-webapp/static/js/main.4e1087bc.chunk.js"
+    "revision": "b69a5683430d51a61fc1",
+    "url": "/b3-companies-webapp/static/js/main.b3dbee48.chunk.js"
   },
   {
     "revision": "ff5ed64e06c3f889881a",
